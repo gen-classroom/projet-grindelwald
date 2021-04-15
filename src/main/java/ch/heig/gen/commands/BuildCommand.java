ccfd716db04fc0c23dd1d8999f69c2e655af161f
fg1989 <fgazzett@gmail.com>
2021-04-15 17:15:23 +0200

    Séparation du projet en packages

--- src/main/java/ch/heig/gen/commands/BuildCommand.java
+++ src/main/java/ch/heig/gen/commands/BuildCommand.java
@@ -1,15 +1,15 @@
-package ch.heig.gen;
+package ch.heig.gen.commands;
 
 import picocli.CommandLine;
 
 @CommandLine.Command(
-        name = "clean",
+        name = "build",
         description = "Not implemented",
         mixinStandardHelpOptions = true,
         version = "1")
-public class CleanCommand implements Runnable {
+public class BuildCommand implements Runnable {
     @Override
     public void run() {
-        System.out.println("'clean' command not implemented");
+        System.out.println("'build' command not implemented");
     }
 }
