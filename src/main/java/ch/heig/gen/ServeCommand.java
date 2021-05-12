9a94fb7873df8f355d8b691dabe4e4f8253a6818
quillasp <saralfddin93@yahoo.fr>
2021-05-12 21:46:08 +0200

    correcting serve command's description

--- src/main/java/ch/heig/gen/ServeCommand.java
+++ src/main/java/ch/heig/gen/ServeCommand.java
@@ -4,7 +4,7 @@ import picocli.CommandLine;
 
 @CommandLine.Command(
         name = "serve",
-        description = "Deploy the static site in production",
+        description = "Serve the static site so that the user can see what it would look like on a real web server",
         mixinStandardHelpOptions = true,
         version = "1")
 public class ServeCommand implements Runnable {
