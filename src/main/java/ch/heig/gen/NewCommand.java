6bf0ae6fce162ca40beaf6ef4840d5f6ff47b764
quillasp <saralfddin93@yahoo.fr>
2021-05-12 21:41:33 +0200

    correcting new and clean commands' description

--- src/main/java/ch/heig/gen/NewCommand.java
+++ src/main/java/ch/heig/gen/NewCommand.java
@@ -4,7 +4,7 @@ import picocli.CommandLine;
 
 @CommandLine.Command(
         name = "new",
-        description = "Initializes the static site by adding a configuration file containing general information",
+        description = "Create a new project in the current directory. This adds the necessary files required by the other commands",
         mixinStandardHelpOptions = true,
         version = "1")
 public class NewCommand implements Runnable {
