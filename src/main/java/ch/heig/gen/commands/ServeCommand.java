5342e5cfbdc35346bdce33bd415bd9d4d9b37020
Hoang Anh Mai <hoanganh.mai@hes-so.ch>
2021-05-13 18:37:41 +0200

    implementing serve command

--- src/main/java/ch/heig/gen/commands/ServeCommand.java
+++ src/main/java/ch/heig/gen/commands/ServeCommand.java
@@ -4,10 +4,11 @@ import picocli.CommandLine;
 
 @CommandLine.Command(
         name = "serve",
-        description = "Not implemented",
+        description = "Open site in default web browser",
         mixinStandardHelpOptions = true,
         version = "1")
 public class ServeCommand implements Runnable {
+    @CommandLine.Parameters(description = "The path to the site in order to be opened in web browser ")
     @Override
     public void run() {
         System.out.println("'serve' command not implemented");
