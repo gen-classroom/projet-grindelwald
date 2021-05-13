74322402c1b291c8f02b6bb788a862fa012685ca
Hoang Anh Mai <hoanganh.mai@hes-so.ch>
2021-05-13 19:55:28 +0200

    finish serve command

--- src/main/java/ch/heig/gen/commands/ServeCommand.java
+++ src/main/java/ch/heig/gen/commands/ServeCommand.java
@@ -2,15 +2,82 @@ package ch.heig.gen.commands;
 
 import picocli.CommandLine;
 
+import java.awt.*;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.ArrayList;
+
+
 @CommandLine.Command(
         name = "serve",
         description = "Open site in default web browser",
         mixinStandardHelpOptions = true,
         version = "1")
 public class ServeCommand implements Runnable {
-    @CommandLine.Parameters(description = "The path to the site in order to be opened in web browser ")
+    @CommandLine.Parameters(description = "The path to the site in order to be opened in web browser.")
+    Path pathToSite;
+
     @Override
     public void run() {
-        System.out.println("'serve' command not implemented");
+        try {
+            File site = pathToSite.toFile();
+            if(site.isDirectory()) {
+                File[] files = site.listFiles();
+                if(files == null) {
+                    throw new IOException("The site has no content.");
+                }
+
+                File dirBuild = findFile("build", site);
+                if(dirBuild == null) {
+                    throw new IOException("The site has not yet been built.");
+                }
+
+                File dirContent = findFile("content", dirBuild);
+                System.out.println(dirContent.getAbsolutePath());
+                if(dirContent == null) {
+                    throw new IOException("There was an error during the site compilation.");
+                }
+
+                ArrayList<File> htmlFiles = findFileByExtension(".html", dirContent);
+                for(File f : htmlFiles) {
+                    Desktop.getDesktop().browse(f.toURI());
+                }
+            }
+        } catch (IOException exception) {
+            System.out.println(exception.getMessage());
+        }
+    }
+
+    private File findFile(String filename, File dir) throws IOException {
+        File[] files = dir.listFiles();
+
+        if(files != null) {
+            for(File f : files) {
+                if(filename.equalsIgnoreCase(f.getName())) {
+                    return f;
+                }
+            }
+        }
+
+        return null;
+    }
+
+    private ArrayList<File> findFileByExtension(String extension, File dir) {
+        ArrayList<File> list = new ArrayList<>();
+        File[] files = dir.listFiles();
+
+        if(files != null) {
+            for(File f : files) {
+                String filename = f.getName();
+                int lastIndexOf = filename.lastIndexOf(".");
+
+                if(filename.substring(lastIndexOf).equals(extension)) {
+                    list.add(f);
+                }
+            }
+        }
+
+        return list;
     }
 }
