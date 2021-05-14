dc7af0118dd56e5549ea918d5c8e441285b79269
Hoang Anh Mai <hoanganh.mai@hes-so.ch>
2021-05-14 11:28:50 +0200

    serve command opening pages for various OS

--- src/main/java/ch/heig/gen/commands/ServeCommand.java
+++ src/main/java/ch/heig/gen/commands/ServeCommand.java
@@ -5,6 +5,7 @@ import picocli.CommandLine;
 import java.awt.*;
 import java.io.File;
 import java.io.IOException;
+import java.net.URI;
 import java.nio.file.Path;
 import java.util.ArrayList;
 
@@ -38,8 +39,9 @@ public class ServeCommand implements Runnable {
                     throw new IOException("There is no page to show");
                 }
 
+                Runtime rt = Runtime.getRuntime();
                 for(File f : htmlFiles) {
-                    Desktop.getDesktop().browse(f.toURI());
+                    openUriInBrowser(f.toURI());
                 }
             } else {
                 throw new IOException("Incorrect path to site");
@@ -79,5 +81,43 @@ public class ServeCommand implements Runnable {
         }
 
         return list;
+    }
+
+    private void openUriInBrowser(URI uri) {
+        String os = System.getProperty("os.name").toLowerCase();
+        Runtime rt = Runtime.getRuntime();
+
+        try{
+
+            if (os.indexOf( "win" ) >= 0) {
+
+                // this doesn't support showing urls in the form of "page.html#nameLink"
+                rt.exec( "rundll32 url.dll,FileProtocolHandler " + uri);
+
+            } else if (os.indexOf( "mac" ) >= 0) {
+
+                rt.exec( "open " + uri);
+
+            } else if (os.indexOf( "nix") >=0 || os.indexOf( "nux") >=0) {
+
+                // Do a best guess on unix until we get a platform independent way
+                // Build a list of browsers to try, in this order.
+                String[] browsers = {"epiphany", "firefox", "mozilla", "konqueror",
+                        "netscape","opera","links","lynx"};
+
+                // Build a command string which looks like "browser1 "url" || browser2 "url" ||..."
+                StringBuffer cmd = new StringBuffer();
+                for (int i=0; i<browsers.length; i++) {
+                    cmd.append((i == 0 ? "" : " || ") + browsers[i] + " \"" + uri + "\" ");
+                }
+
+                rt.exec(new String[] { "sh", "-c", cmd.toString() });
+
+            } else {
+                throw new Exception("The program doesn't support the OS.");
+            }
+        } catch (Exception e){
+            System.out.println(e.getMessage());
+        }
     }
 }
